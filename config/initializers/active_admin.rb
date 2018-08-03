286c8e68c59c529aabe7b068ec6d0fbf23d015f2
donkim <dickings@likelion.org>
2018-08-03 23:23:10 +0900

    어드민 설치

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,8 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "영양해"
-
+  config.site_title_link  = "/"
+  config.site_title_image = "adminlogo.png"
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
