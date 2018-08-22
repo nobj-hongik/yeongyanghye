65d586c2d8dad69fdb1ff9c50f5981da81075214
nobj <shgustjd6262@naver.com>
2018-08-22 09:33:01 +0000

    머지 준비

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" , :registrations=>'registrations'  }
-  
+  get 'home/mentor'
   get 'auth/:provider/callback', to: 'users#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'users#destroy', as: 'signout'
